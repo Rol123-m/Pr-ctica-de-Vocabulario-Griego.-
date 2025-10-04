d45b8c6aa5022977eb12adc9c08d27bf180d0b1f
Rol123-m <rol123-m.github.io.com>
2025-10-04 19:12:56 -0400

    Updates

--- src/components/Layout/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer>
-      <p>© Ministerio "Vivos para Servir" - Sección Educativa</p>
-    </footer>
-  );
-};
-
-export default Footer;
--- src/utils/constants.js
+++
@@ -1,2 +0,0 @@
-export const APP_NAME = "Aprende Griego Koiné";
-export const APP_DESCRIPTION = "Ministerio Vivos para Servir - Sección Educativa";
